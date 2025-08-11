cf3a3a715b0e2829d7c2c60e8a3d5d8473a4a670
bejo-PaulVerbruggen <paul.verbruggen@bejo.nl>
2025-08-11 15:55:22 +0200

    eod

--- inspect_images.py
+++ inspect_images.py
@@ -1,7 +1,8 @@
 import cv2
 import os
 import pandas as pd
-
+import re 
+import numpy as np
 #define label source
 label_src = '/mnt/share/tempdata/Pauls_tijdelijke_rotzooi/DCDL_labelfiles/REDBEET/detailed_labels_side.xlsx'
 
@@ -13,15 +14,24 @@ for _,row in metadata.iterrows():
     side = row['side_prediction']
     score = row['score']
     if row['imagefile'] != 'file not present':
-        image = cv2.imread(row['imagefile'])
-        cv2.putText(image,f'no: {str(no_seedlings)}',(10,15),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1,cv2.LINE_AA)
-        cv2.putText(image,f'no healthy: {str(healthy_seedlings)}',(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1,cv2.LINE_AA)
-        cv2.putText(image,f'side: {str(side)}',(10,45),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1,cv2.LINE_AA)
-        cv2.putText(image,f'score: {str(score)}',(10,60),cv2.FONT_HERSHEY_SIMPLEX,0.5,(255,255,255),1,cv2.LINE_AA)
+        image = np.invert(cv2.imread(row['imagefile']))
+        imagefile_VIS = re.sub("XRAY","VIS",row['imagefile'])
+        image_VIS = cv2.imread(imagefile_VIS)
+        #imagefile_CF = re.sub("XRAY","CF",row['imagefile'])
+        #image_CF = cv2.imread(imagefile_CF)
+        #image_CF = cv2.resize(image_CF,(500,500))
+        text_color = (0,0,0)
+        cv2.putText(image,f'no: {str(no_seedlings)}',(10,15),cv2.FONT_HERSHEY_SIMPLEX,0.5,text_color,1,cv2.LINE_AA)
+        cv2.putText(image,f'no healthy: {str(healthy_seedlings)}',(10,30),cv2.FONT_HERSHEY_SIMPLEX,0.5,text_color,1,cv2.LINE_AA)
+        cv2.putText(image,f'side: {str(side)}',(10,45),cv2.FONT_HERSHEY_SIMPLEX,0.5,text_color,1,cv2.LINE_AA)
+        cv2.putText(image,f'score: {str(score)}',(10,60),cv2.FONT_HERSHEY_SIMPLEX,0.5,text_color,1,cv2.LINE_AA)
         cv2.namedWindow('test')        
         cv2.moveWindow('test', 40,30)
-        cv2.imshow('test',image)
+        try:
+            final_img = np.hstack((image_VIS,image))
+        except Exception as e:
+            print(f"Failed with exception: {e}")
+            final_img = image
+        cv2.imshow('test',final_img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-
-
